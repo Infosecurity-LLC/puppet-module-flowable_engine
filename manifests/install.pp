26930b083d19012cdec6fb63952326222abc3936
Alexander Slyotov <sletov@in4security.com>
2020-08-28 18:16:34 +0300

    Flowable_engine ISOC

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,25 +17,21 @@ class flowable_engine::install(
     ensure => present,
   }
 
-#  exec { $source_file_name:
-#    environment => [
-#      "http_proxy=$proxy_url",
-#      "https_proxy=$proxy_url",
-#    ],
-#    command => "/bin/wget -o ${webapps_folder}/${source_file_name} $source_file_url",
-#  }
+  exec { $source_file_name:
+    environment => [
+      "http_proxy=$proxy_url",
+      "https_proxy=$proxy_url",
+    ],
+    command => "/bin/wget -o ${webapps_folder}/${source_file_name} $source_file_url",
+  }
 
   archive { "${webapps_folder}/${source_file_name}":
     ensure          => present,
+    source          => "${webapps_folder}/${source_file_name}",
     extract         => true,
-    path            => "${webapps_folder}/${source_file_name}",
     extract_path    => $webapps_folder,
     extract_command => "unzip -j %s *.war",
-    source          => $source_file_url,
-    proxy_server    => $proxy_url,
-    proxy_type      => 'https',
     cleanup         => true,
-    require         => Package['wget', 'unzip'],
   }
 
 }
