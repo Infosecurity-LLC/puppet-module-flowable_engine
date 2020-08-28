bfbb50d262e1e2e890515bced0f3bca8669c90c6
Alexander Slyotov <sletov@in4security.com>
2020-08-28 16:19:15 +0300

    Flowable_engine ISOC

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,13 +17,6 @@ class flowable_engine::install(
     ensure => present,
   }
 
-#  file { $webapps_folder:
-#    ensure => directory,
-#    owner  => 'tomcat',
-#    group  => 'tomcat',
-#    mode   => '0755',
-#  }
-
   archive { $webapps_folder:
     ensure          => present,
     provider        => 'wget',
@@ -31,27 +24,8 @@ class flowable_engine::install(
     extract_path    => $webapps_folder,
     extract_command => "unzip -j %s *.war",
     source          => $source_file_url,
-    creates         => "${webapps_folder}/ROOT",
     cleanup         => true,
-    require  => Package['wget', 'unzip'],
+    require         => Package['wget', 'unzip'],
   }
-
-
-#  archive { "${source_file_name}":
-#    ensure          => present,
-#    extract         => true,
-#    extract_command => "unzip -j %s *.war",
-#    extract_path    => "${webapps_folder}",
-#    source          => "${source_file_url}",
-#    creates         => "${webapps_folder}/ROOT",
-#    cleanup         => true,
-#    proxy_server    => "${proxy_url}",
-#    before          => File["${webapps_folder}"],
-#    require         => [
-#      Package[$package_name],
-#      Package['wget'],
-#      Package['unzip'],
-#    ],
-#  }
 
 }
